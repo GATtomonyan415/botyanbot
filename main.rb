5bc8ff6d5855b08268834f98097539074fda83a2
GATtomonyan415 <70007651+GATtomonyan415@users.noreply.github.com>
2022-01-30 13:53:32 +0900

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,16 @@
+require "discorb"
+
+client = Discorb::Client.new
+
+client.once :ready do
+  puts "Logged in as #{client.user}"
+end
+
+client.on :message do |message|
+  next if message.author.bot?
+  next unless message.content == "/ping"
+
+  message.channel.post("pong!")
+end
+
+client.run(ENV["DISCORD_BOT_TOKEN"])
